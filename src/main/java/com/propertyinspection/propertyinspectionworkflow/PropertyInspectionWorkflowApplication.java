11bd49fe79ddf45f6e92302c5a5ee60c3a08d230
Sneha17 <snehag1795@gmail.com>
2026-08-20 09:41:53 +0530

    Initial Spring Boot project skeleton with Web, JPA, H2, Validation

---
+++ src/main/java/com/propertyinspection/propertyinspectionworkflow/PropertyInspectionWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.propertyinspection.propertyinspectionworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyInspectionWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyInspectionWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertyinspection/propertyinspectionworkflow/PropertyInspectionWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertyinspection.propertyinspectionworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyInspectionWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
